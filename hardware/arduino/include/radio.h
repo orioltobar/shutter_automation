183d63311cd66f7cb962b250a5bba3472a1dba35
Oriol Tobar <oriol.tobar@gmail.com>
2025-09-24 12:41:32 +0200

    Modified trigger up and trigger down functions to use blind type.

--- hardware/arduino/include/radio.h
+++ hardware/arduino/include/radio.h
@@ -1,9 +1,12 @@
 #ifndef RADIO_H
 #define RADIO_H
+#include "common.h"
 
     void init_radio(unsigned char pin_radio);
     void send_up_signal();
+    void send_up_signal_with_blind(BlindType blind_type);
     void send_down_signal();
+    void send_down_signal_with_blind(BlindType blind_type);
     void send_stop_signal();
 
 #endif
